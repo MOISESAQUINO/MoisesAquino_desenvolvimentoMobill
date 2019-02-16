766f50a2e7ac98dcb2a83ec4ebc8c8e22e5ad7d6
26146152 <aquinomoises03@gmail.com>
2019-02-15 21:32:50 -0300

    atualizado

---
+++ src/Exercicio5/aniversario.java
@@ -0,0 +1,46 @@
+package Exercicio5;
+
+public class aniversario {
+    String data, sexo, endereco, horario;
+    int quantidadePessoa;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public int getQuantidadePessoa() {
+        return quantidadePessoa;
+    }
+
+    public void setQuantidadePessoa(int quantidadePessoa) {
+        this.quantidadePessoa = quantidadePessoa;
+    }
+}
--- src/Exercico4/Quarto.java
+++ src/Exercico4/Quarto.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 public class Quarto {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
-        System.out.println(" entre com o numero ");
-        int num = s.nextInt();
 
-        if (num >= 0 && (num <= 100)){
+        int num = 0;
 
-            System.out.println(" o numero fica entre 0 e 100");
+       while (num >= 0 &&  num <= 100) {
+           System.out.println(" entre com o numero ");
+           num = s.nextInt();
 
-        }else {
-            System.out.println(" o numero nao esta entre 0 e 100");
-        }
+           if (num >= 0 && num <= 100) {
 
+               System.out.println(" o numero fica entre 0 e 100");
 
+           } else {
+               System.out.println(" o numero nao esta entre 0 e 100");
+           }
+
+       }
 
     }
 }
